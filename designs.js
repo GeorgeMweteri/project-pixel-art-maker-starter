81432c6b6656bc5614b9c3a19f79392be6204f60
MweteriGeorge <mweterigeorge@gmail.com>
2018-07-01 21:26:00 +0300

    first commit in the project

--- designs.js
+++ designs.js
@@ -4,7 +4,47 @@
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
+$(function() {
+    'use strict';
 
-// Your code goes here!
+
+    // dom pointers
+    const canvasEl = $('#pixel_canvas');
+    const sizePickerEl = $('#sizePicker');
+
+    // Set color
+    function setColor() {
+        const colorVal = $('#colorPicker').val();
+        return colorVal;
+    }
+
+
+    // delete grid
+    function deleteGrid() {
+        $(canvasEl).empty();
+    }
+
+
+    // create grid
+    function makeGrid() {
+
+        // Before adding new rows to the table, delete the existing ones first
+        deleteGrid();
+
+        const width = $('#input_width').val();
+        const height = $('#input_height').val();
+
+        for (let r = 0; r < width; r++) {
+            const row = $('<tr></tr>');
+
+            for (let c = 0; c < height; c++) {
+                const cell = $('<td></td>');
+                $(row).append(cell);
+
+                // Event listener that sets background-color for each pixel
+                $(cell).click(function() {
+                    cell.css('background-color', setColor());
+                });
+            }
 
 }
